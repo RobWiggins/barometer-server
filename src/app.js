423fcef03aa8cbfde4491f0021935d146799ff3c
RobWiggins <wigginsro11@gmail.com>
2022-08-11 16:40:34 -0400

    added corsOptions to endpoint handlers

--- src/app.js
+++ src/app.js
@@ -22,8 +22,8 @@ app.use(morgan(morganOption));
 app.use(cors(corsOptions));
 app.use(helmet());
 
-app.use('/tweets/queries', tweetsRouter);
-app.use('/queries/history', historyRouter);
+app.use('/tweets/queries', cors(corsOptions), tweetsRouter);
+app.use('/queries/history', cors(corsOptions), historyRouter);
 
 app.use(function errorHandler(error, req, res, next) {
   let response;
