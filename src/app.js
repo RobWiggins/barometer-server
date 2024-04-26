c0d16233bc6f175fa0f5e8d4b376a55aa014c78c
RobWiggins <wigginsro11@gmail.com>
2024-04-26 15:39:59 -0400

    testing adding generic cors() back

--- src/app.js
+++ src/app.js
@@ -15,7 +15,7 @@ const morganOption = NODE_ENV === 'production' ? 'tiny' : 'dev';
 
 app.use(morgan(morganOption));
 app.use(helmet());
-// app.use(cors())
+app.use(cors())
 app.options('*', cors())
 
 app.use('/tweets/queries', tweetsRouter);
