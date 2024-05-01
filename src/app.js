483f63b7bcbfeaeb49a89b4d03085eb9e2c2e07a
RobWiggins <wigginsro11@gmail.com>
2024-05-01 12:01:05 -0400

    messing with cors origin, added postgrator to regular dependencies

--- src/app.js
+++ src/app.js
@@ -15,8 +15,12 @@ const morganOption = NODE_ENV === 'production' ? 'tiny' : 'dev';
 
 app.use(morgan(morganOption));
 app.use(helmet());
-app.use(cors())
-app.options('*', cors())
+// app.use(cors())
+const corsOptions = {
+  origin: '*'
+}
+// app.options({ origin: '*'}, cors())
+app.use(cors(corsOptions))
 
 app.use('/tweets/queries', tweetsRouter);
 app.use('/queries/history', historyRouter);
