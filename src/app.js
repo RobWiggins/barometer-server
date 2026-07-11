bdf93581fa8065c384b292b311768c3b65021b16
robert wiggins <wigginsro11@gmail.com>
2026-07-11 15:51:54 -0400

    added specific CORS allowed origins

--- src/app.js
+++ src/app.js
@@ -13,15 +13,19 @@ const app = express();
 
 const morganOption = NODE_ENV === 'production' ? 'tiny' : 'dev';
 
+const allowedOrigins = [
+  'http://localhost:3000',
+  'https://barometerapp.vercel.app',
+];
+app.use(
+  cors({
+    origin: allowedOrigins,
+    methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+  })
+);
 app.use(morgan(morganOption));
 app.use(helmet());
-app.use(cors())
-// const corsOptions = {
-//   origin: '*',
-//   allowedHeaders: ['Content-Type', 'Authorization']
-// }
-// app.options({ origin: '*'}, cors())
-// app.use(cors(corsOptions))
 
 app.use('/tweets/queries', tweetsRouter);
 app.use('/queries/history', historyRouter);
