9ad7028e88e12bd18d14cfcf574216a789817826
robert wiggins <wigginsro11@gmail.com>
2026-07-12 11:46:40 -0400

    attempt to fix CORS error

--- src/app.js
+++ src/app.js
@@ -13,19 +13,35 @@ const app = express();
 
 const morganOption = NODE_ENV === 'production' ? 'tiny' : 'dev';
 
+app.use(morgan(morganOption));
+app.use(helmet());
+
 const allowedOrigins = [
   'http://localhost:3000',
   'https://barometerapp.vercel.app',
 ];
+// app.use(
+//   cors({
+//     origin: allowedOrigins,
+//     methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
+//     allowedHeaders: ["Content-Type", "Authorization"],
+//   })
+// );
+
 app.use(
   cors({
-    origin: allowedOrigins,
+    origin(origin, callback) {
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true);
+        return;
+      }
+
+      callback(new Error(`CORS blocked origin: ${origin}`));
+    },
     methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"],
   })
 );
-app.use(morgan(morganOption));
-app.use(helmet());
 
 app.use('/tweets/queries', tweetsRouter);
 app.use('/queries/history', historyRouter);
